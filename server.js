25ff087e0b138d3d3526d0e3c740f8751935b93c
Ryan Manse <ryanmanse@gmail.com>
2019-05-13 06:30:23 -0500

    adding api controls

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send(`API Running`));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
